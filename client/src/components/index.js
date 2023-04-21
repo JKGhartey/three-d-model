15f7798a67073cc5411b8c99f0d3d7384cc9391a
JKGhartey <79650095+JKGhartey@users.noreply.github.com>
2023-04-21 16:27:18 +0100

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,20 @@
+import CustomButton from "./CustomButton";
+import AiPicker from "./AiPicker";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+
+export {
+  CustomButton,
+  AiPicker,
+  FilePicker,
+  ColorPicker,
+  Tab,
+  Shirt,
+  Backdrop,
+  CameraRig,
+};
